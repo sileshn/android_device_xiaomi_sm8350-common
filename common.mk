6aa18b51242d6718dd4a9328f16bbaea1edb5738
Nolen Johnson <johnsonnolen@gmail.com>
2021-02-24 20:56:15 -0500

    sm8350-common: Drop PRODUCT_PACKAGES_DEBUG entry
    * unnecessary
    
    Change-Id: I22ae2c744ec55168b8367c826e55110e7534466e

--- common.mk
+++ common.mk
@@ -106,9 +106,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
-
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
@@ -438,9 +435,6 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
     update_verifier
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
 
 # USB
 PRODUCT_PACKAGES += \
