a4901db5865015250594f0dd5c12aeca2351fe4d
basamaryan <basam.aryan@gmail.com>
2024-04-22 19:09:53 +0200

    sm8350-common: Use the common Xiaomi FCM
    Change-Id: I0ca395bc5c23c26e1546d3dd1839592530cc0e3f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,10 +65,9 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/hidl/device_framework_compatibility_matrix.xml \
-    $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
