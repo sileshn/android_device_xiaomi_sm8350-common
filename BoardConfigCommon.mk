3e071b4c3252c94db6bcdd5cac5398612335995b
Michael Bestas <mkbestas@lineageos.org>
2024-08-16 03:55:37 +0300

    sm8350-common: Bump LOC_HIDL_VERSION to 4.2
    Change-Id: Ic961685f4a1887a2053ff08455ea3c3d0958ac86

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,7 +60,7 @@ HWUI_COMPILE_FOR_PERF := true
 
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
-LOC_HIDL_VERSION := 4.0
+LOC_HIDL_VERSION := 4.2
 
 # HIDL
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
