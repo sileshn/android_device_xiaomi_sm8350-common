664a801f535e4aea567e77a9f1ffa286d9d75baf
Giovanni Ricca <giovanniricca@proton.me>
2025-03-29 21:49:17 +0100

    fixup! sm8350-common: Mark CNE symlink target as required package
    Change-Id: I54480890f80789596dafe5b1fc3a1866d2baa989

--- common.mk
+++ common.mk
@@ -359,7 +359,7 @@ PRODUCT_COPY_FILES += \
 
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
-    libvndfwk_detect_jni.qti.vendor:64 # Needed by CNE app
+    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
 # Recovery
 PRODUCT_COPY_FILES += \
